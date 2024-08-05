d45baf03e862b3ed467b4706ec8f7e09553fa910
DaniCoimbra <dcsalomao2000@gmail.com>
2024-08-05 00:16:24 +0000

    updates

--- myproject/survey_demo/__init__.py
+++ myproject/survey_demo/__init__.py
@@ -7,98 +7,115 @@ class C(BaseConstants):
     NAME_IN_URL = 'survey_demo'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 8
-    INVESTMENTS_FILE = '_static/Investments.csv'
-    R_FILE = '_static/R.csv'
+    DISTRIBUTION_FILE = '_static/distributions.csv'
+    INVESTMENT_MATRIX_FILE = '_static/InvestmentMatrix_0234.csv'
 
 investments = None
 R = None
 
 class Subsession(BaseSubsession):
-    investments = models.LongStringField()
-    R = models.LongStringField()
+    distributions = models.LongStringField()
+    investment_matrix = models.LongStringField()
 
 class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
-    risk = models.IntegerField(
-        choices=[(i, f'{i}') for i in range(11)],
-        widget=widgets.RadioSelectHorizontal
-    )
+    investment_amount = models.IntegerField(initial=0, min=0, max=100)
     lottery_return = models.FloatField(initial=0)
     risky_return = models.FloatField(initial=0)
     risk_free_return = models.FloatField(initial=0)
-    investment_amount = models.FloatField(initial=0)
     risk_free_investment = models.FloatField(initial=0)
     cumulative_return = models.FloatField(initial=0)
     lottery_round = models.IntegerField(initial=1)
 
 # FUNCTIONS
 def creating_session(subsession: Subsession):
-    investments_df = pd.read_csv(C.INVESTMENTS_FILE)
-    R_df = pd.read_csv(C.R_FILE)
-
-    R_df = R_df.loc[:, (R_df != 0).any(axis=0)]
+    investments_df = pd.read_csv(C.INVESTMENT_MATRIX_FILE)
+    distributions_df = pd.read_csv(C.DISTRIBUTION_FILE)
 
     # JSON strings
-    subsession.investments = json.dumps(investments_df.values.tolist())
-    subsession.R = json.dumps(R_df.values.tolist())
+    subsession.distributions = json.dumps(distributions_df.to_dict(orient='list'))
+    subsession.investment_matrix = json.dumps(investments_df.to_dict(orient='list'))
 
 def calculate_lottery_return(player: Player):
-    investments = json.loads(player.subsession.investments)
-    R = json.loads(player.subsession.R)
-    
-    # Investment ammount
-    risk_level = player.risk
-    investment_amount = investments[risk_level][player.lottery_round - 1]
-    
-    # Random return from R
-    column_values = [row[player.lottery_round - 1] for row in R if row[player.lottery_round - 1] != 0]
-    random_return = random.choice(column_values)
+    distributions = json.loads(player.subsession.distributions)
+    investment_matrix = json.loads(player.subsession.investment_matrix)
 
-    # Risky and Risk Free Returns
-    risky_return = investment_amount * random_return
-    risk_free_return = (100 - investment_amount) * (1 + 0.03 if player.lottery_round % 2 == 1 else 0.10)
     
-    player.investment_amount = investment_amount
-    player.risk_free_investment = 100 - investment_amount
+    investment_amount = player.investment_amount
+    
+    column_names = [
+        'mu10sigma25rf0', 'mu10sigma25rf2', 'mu10sigma25rf3', 'mu10sigma25rf4',
+        'mu10sigma15rf0', 'mu10sigma15rf2', 'mu10sigma15rf3', 'mu10sigma15rf4',
+        'mu13sigma25rf0', 'mu13sigma25rf2', 'mu13sigma25rf3', 'mu13sigma25rf4',
+        'mu13sigma15rf0', 'mu13sigma15rf2', 'mu13sigma15rf3', 'mu13sigma15rf4'
+    ]
+
+    column_name = column_names[(player.lottery_round - 1) % len(column_names)]
+    risky_investment = investment_matrix[column_name][investment_amount]
+
+    risk_free_investment = 100 - risky_investment
+
+    distribution_columns = ['a', 'b', 'c', 'd']
+    distribution_column = distribution_columns[(player.lottery_round - 1) % len(distribution_columns)]
+    random_return = random.choice(distributions[distribution_column])
+    
+    # Calculate returns
+    risky_return = risky_investment * random_return
+    risk_free_rate = int(column_name.split('rf')[-1]) / 100
+    risk_free_return = (risk_free_investment) * (1 + risk_free_rate)
+    
     player.risky_return = risky_return
     player.risk_free_return = risk_free_return
-    player.lottery_return = risky_return + risk_free_return
+    player.lottery_return = risky_return + risk_free_return - 100
     
     player.cumulative_return += player.lottery_return
+
+    # debug info
+    print(f"Round {player.lottery_round} - Investment Amount: {100}")
+    print(f"Risky Investment: {risky_investment}")
+    print(f"Risk-Free Investment: {risk_free_investment}")
+    print(f"Selected Column: {column_name}")
+    print(f"Distribution Column: {distribution_column}")
+    print(f"Random Return: {random_return}")
+    print(f"Risk-Free Rate: {risk_free_rate}")
+    print(f"Risky Return: {risky_return}")
+    print(f"Risk-Free Return: {risk_free_return}")
+    print(f"Total Lottery Return: {player.lottery_return}")
+    print(f"Cumulative Return: {player.cumulative_return}")
+    print("\n")
     
-    return investment_amount, random_return
+    return risky_investment, random_return, risk_free_investment, risk_free_rate
 
 # PAGES
-class RiskSelection(Page):
+class InvestmentSelection(Page):
     form_model = 'player'
-    form_fields = ['risk']
+    form_fields = ['investment_amount']
 
     @staticmethod
     def vars_for_template(player: Player):
-        risks = [{'i': i, 'investment': i * 10} for i in range(11)]
         return {
-            'risks': risks,
-            'previous_risk': player.risk if player.lottery_round > 1 else None
+            'previous_investment': player.investment_amount if player.lottery_round > 1 else None
         }
 
 class Lottery(Page):
     @staticmethod
     def vars_for_template(player: Player):
-        investment_amount, random_return = calculate_lottery_return(player)
+        risky_investment, random_return, risk_free_investment, risk_free_rate = calculate_lottery_return(player)
+        lottery_types = ['A', 'B', 'C', 'D']
+        lottery_type = lottery_types[(player.lottery_round - 1) % len(lottery_types)]
         return {
             'lottery_round': player.lottery_round,
-            'investment_amount': investment_amount,
-            'risk_free_investment': player.risk_free_investment,
-            'random_return': random_return,
-            'risk_free_rate': 3 if player.lottery_round % 2 == 1 else 10,
-            'lottery_type': 'A' if player.lottery_round <= 4 else 'B',
+            'risky_investment': risky_investment,
+            'risk_free_investment': risk_free_investment,
+            'random_return': "{:.2f}".format(random_return * 100),
+            'risk_free_rate': risk_free_rate * 100,
+            'lottery_type': lottery_type,
         }
 
     @staticmethod
     def before_next_page(player: Player, timeout_happened):
-        calculate_lottery_return(player)
         player.lottery_round += 1
 
 class LotteryResults(Page):
@@ -106,12 +123,8 @@ class LotteryResults(Page):
     def vars_for_template(player: Player):
         return {
             'investment_amount': player.investment_amount,
-            'risk_free_investment': player.risk_free_investment,
-            'risky_return': player.risky_return,
-            'risk_free_return': player.risk_free_return,
-            'total_return': player.lottery_return,
+            'lottery_return': player.lottery_return,
             'cumulative_return': player.cumulative_return,
-            'risk_free_rate': 3 if player.lottery_round % 2 == 1 else 10,
         }
 
 class Result(Page):
@@ -121,6 +134,12 @@ class Result(Page):
             'cumulative_return': player.cumulative_return,
         }
 
-page_sequence = [RiskSelection, Lottery, LotteryResults, Lottery, LotteryResults, Result, 
-                 RiskSelection, Lottery, LotteryResults, Lottery, LotteryResults, Result,
-                ]
+page_sequence = [
+    InvestmentSelection, 
+    Lottery, LotteryResults, 
+    Lottery, LotteryResults, 
+    Lottery, LotteryResults, 
+    Lottery, LotteryResults, 
+    InvestmentSelection,
+    Result
+]
