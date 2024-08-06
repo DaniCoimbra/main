f99720ce8fdc90214a67d791cefd4a09bd7d36d4
DaniCoimbra <dcsalomao2000@gmail.com>
2024-08-06 01:46:33 +0000

    updates

--- myproject/survey_demo/__init__.py
+++ myproject/survey_demo/__init__.py
@@ -3,6 +3,8 @@ import random
 import pandas as pd
 import json
 
+from survey_demo.lottery import L
+
 class C(BaseConstants):
     NAME_IN_URL = 'survey_demo'
     PLAYERS_PER_GROUP = None
@@ -12,6 +14,8 @@ class C(BaseConstants):
 
 investments = None
 R = None
+
+lotteries = []
 
 class Subsession(BaseSubsession):
     distributions = models.LongStringField()
@@ -25,8 +29,8 @@ class Player(BasePlayer):
     lottery_return = models.FloatField(initial=0)
     risky_return = models.FloatField(initial=0)
     risk_free_return = models.FloatField(initial=0)
+    risky_investment = models.FloatField(initial=0)
     risk_free_investment = models.FloatField(initial=0)
-    cumulative_return = models.FloatField(initial=0)
     lottery_round = models.IntegerField(initial=1)
 
 # FUNCTIONS
@@ -57,21 +61,26 @@ def calculate_lottery_return(player: Player):
 
     risk_free_investment = 100 - risky_investment
 
-    distribution_columns = ['a', 'b', 'c', 'd']
+    distribution_columns = ['A', 'B', 'C', 'D']
     distribution_column = distribution_columns[(player.lottery_round - 1) % len(distribution_columns)]
     random_return = random.choice(distributions[distribution_column])
     
+    mean = int(column_name.split('mu')[1].split('sigma')[0])
+    std_dv = int(column_name.split('sigma')[1].split('rf')[0])
+
     # Calculate returns
     risky_return = risky_investment * random_return
     risk_free_rate = int(column_name.split('rf')[-1]) / 100
     risk_free_return = (risk_free_investment) * (1 + risk_free_rate)
+
+    outcome = risky_return + risk_free_return
     
+    player.risky_investment = risky_investment
+    player.risk_free_investment = risk_free_investment
     player.risky_return = risky_return
     player.risk_free_return = risk_free_return
-    player.lottery_return = risky_return + risk_free_return - 100
+    player.lottery_return = outcome
     
-    player.cumulative_return += player.lottery_return
-
     # debug info
     print(f"Round {player.lottery_round} - Investment Amount: {100}")
     print(f"Risky Investment: {risky_investment}")
@@ -82,11 +91,12 @@ def calculate_lottery_return(player: Player):
     print(f"Risk-Free Rate: {risk_free_rate}")
     print(f"Risky Return: {risky_return}")
     print(f"Risk-Free Return: {risk_free_return}")
-    print(f"Total Lottery Return: {player.lottery_return}")
-    print(f"Cumulative Return: {player.cumulative_return}")
+    print(f"Total Return: {player.lottery_return}")
     print("\n")
+
+    lotteries.append(L(distribution_column, random_return, mean, std_dv, risky_investment, outcome))    
     
-    return risky_investment, random_return, risk_free_investment, risk_free_rate
+    return mean, std_dv, risk_free_rate
 
 # PAGES
 class InvestmentSelection(Page):
@@ -102,16 +112,14 @@ class InvestmentSelection(Page):
 class Lottery(Page):
     @staticmethod
     def vars_for_template(player: Player):
-        risky_investment, random_return, risk_free_investment, risk_free_rate = calculate_lottery_return(player)
+        mean, std_dv, rf = calculate_lottery_return(player)
         lottery_types = ['A', 'B', 'C', 'D']
         lottery_type = lottery_types[(player.lottery_round - 1) % len(lottery_types)]
         return {
-            'lottery_round': player.lottery_round,
-            'risky_investment': risky_investment,
-            'risk_free_investment': risk_free_investment,
-            'random_return': "{:.2f}".format(random_return * 100),
-            'risk_free_rate': risk_free_rate * 100,
             'lottery_type': lottery_type,
+            'risk_free_rate': round(rf * 100, 2),
+            'mean': round(mean, 2),
+            'std_dv': round(std_dv, 2),
         }
 
     @staticmethod
@@ -122,16 +130,43 @@ class LotteryResults(Page):
     @staticmethod
     def vars_for_template(player: Player):
         return {
-            'investment_amount': player.investment_amount,
-            'lottery_return': player.lottery_return,
-            'cumulative_return': player.cumulative_return,
+            'lottery_type': lotteries[-1].type,
+            'mean': round(lotteries[-1].mean, 2),
+            'std_dv': round(lotteries[-1].std_dv, 2),
+            'risky_investment': round(lotteries[-1].risk, 2),
+            'risk_free_investment': round(100 - lotteries[-1].risk, 2),
         }
 
 class Result(Page):
     @staticmethod
     def vars_for_template(player: Player):
+        lottery_4 = lotteries.pop()
+        lottery_3 = lotteries.pop()
+        lottery_2 = lotteries.pop()
+        lottery_1 = lotteries.pop()
+
         return {
-            'cumulative_return': player.cumulative_return,
+            'lottery_1_risky': round(lottery_1.risk, 2),
+            'lottery_1_rf': round(100 - lottery_1.risk, 2),
+            'lottery_1_random': round(lottery_1.random, 2),
+            'lottery_1_outcome': round(lottery_1.outcome, 2),
+
+            'lottery_2_risky': round(lottery_2.risk, 2),
+            'lottery_2_rf': round(100 - lottery_2.risk, 2),
+            'lottery_2_random': round(lottery_2.random, 2),
+            'lottery_2_outcome': round(lottery_2.outcome, 2),
+
+            'lottery_3_risky': round(lottery_3.risk, 2),
+            'lottery_3_rf': round(100 - lottery_3.risk, 2),
+            'lottery_3_random': round(lottery_3.random, 2),
+            'lottery_3_outcome': round(lottery_3.outcome, 2),
+
+            'lottery_4_risky': round(lottery_4.risk, 2),
+            'lottery_4_rf': round(100 - lottery_4.risk, 2),
+            'lottery_4_random': round(lottery_4.random, 2),
+            'lottery_4_outcome': round(lottery_4.outcome, 2),
+
+            'cumulative_return': round(lottery_1.outcome + lottery_2.outcome + lottery_3.outcome + lottery_4.outcome, 2)
         }
 
 page_sequence = [
@@ -140,6 +175,5 @@ page_sequence = [
     Lottery, LotteryResults, 
     Lottery, LotteryResults, 
     Lottery, LotteryResults, 
-    InvestmentSelection,
-    Result
+    Result,
 ]
---
+++ myproject/survey_demo/lottery.py
@@ -0,0 +1,8 @@
+class L:
+    def __init__(self, type, random, mean, std_dv, risk_investment, outcome):
+        self.type = type
+        self.random = random
+        self.mean = mean
+        self.std_dv = std_dv
+        self.risk = risk_investment
+        self.outcome = outcome
