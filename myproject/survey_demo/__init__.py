86a097502ff7946cd8159a1969bd9e1de838dd62
DaniCoimbra <dcsalomao2000@gmail.com>
2024-08-24 03:23:45 +0000

    added questions and updated

--- myproject/survey_demo/__init__.py
+++ myproject/survey_demo/__init__.py
@@ -15,28 +15,90 @@ class C(BaseConstants):
     DISTRIBUTION_FILE = '_static/distributions.csv'
     INVESTMENT_MATRIX_FILE = '_static/InvestmentMatrix_0234.csv'
     TREATMENTS = ['treatment_1', 'treatment_2', 'treatment_3']
-
-investments = None
-R = None
+    LABELS = [
+        (1, "Strongly Disagree"),
+        (2, "Disagree"),
+        (3, "Somewhat Disagree"),
+        (4, "Neither agree nor disagree"),
+        (5, "Somewhat Agree"),
+        (6, "Agree"),
+        (7, "Strongly Agree"),
+        (8, "I do not know")
+    ]
 
 class Subsession(BaseSubsession):
-    distributions = models.LongStringField()
-    investment_matrix = models.LongStringField()
+    pass
 
 class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
     investment_amount = models.IntegerField(initial=0, min=0, max=100)
-    lottery_return = models.FloatField(initial=0)
-    risky_return = models.FloatField(initial=0)
-    risk_free_return = models.FloatField(initial=0)
-    risky_investment = models.FloatField(initial=0)
-    risk_free_investment = models.FloatField(initial=0)
     lottery_round = models.IntegerField(initial=1)
     lotteries = models.LongStringField(initial='[]')
     treatment = models.StringField(initial="")
     chosen_lotteries = models.LongStringField(initial='[]')
+    investment_choices = models.LongStringField(initial='[]')
+    risky_draw = models.LongStringField(initial='[]')
+    q1 = models.IntegerField(
+        choices=C.LABELS,
+        widget=widgets.RadioSelectHorizontal,
+        label="I find it easy to get technology to do what I want it to do:"
+    )
+    q2 = models.IntegerField(
+        choices=C.LABELS,
+        widget=widgets.RadioSelectHorizontal,
+        label="Using technology helps me accomplish tasks more quickly:"
+    )
+    q3 = models.IntegerField(
+        choices=C.LABELS,
+        widget=widgets.RadioSelectHorizontal,
+        label="I find it easy to recover from errors encountered while using technology:"
+    )
+    q4 = models.IntegerField(
+        choices=[
+            (1, "Adventurous"),
+            (2, ""),
+            (3, ""),
+            (4, ""),
+            (5, ""),
+            (6, ""),
+            (7, ""),
+            (8, "Prudent"),
+            (9, "I do not know")
+        ],
+        widget=widgets.RadioSelectHorizontal,
+        label="In terms of financial decisions, what is your attitude?"
+    )
+    q5 = models.IntegerField(
+        choices=[
+            (1, "More than $102"),
+            (2, "Exactly $102"),
+            (3, "Less than $102"),
+            (4, "Do not know/ refuse to answer"),
+        ],
+        widget=widgets.RadioSelectHorizontal,
+        label="Suppose you had $100 in a savings account and the interest rate was 2% per year.  After 5 years, how much do you think you would have in the account if you left the money to grow?"
+    )
+    q6 = models.IntegerField(
+        choices=[
+            (1, "More than today with the money in this account"),
+            (2, "Exactly the same as today with the money in this account"),
+            (3, "Less than today with the money in this account"),
+            (4, "Do not know/ refuse to answer"),
+        ],
+        widget=widgets.RadioSelectHorizontal,
+        label="Imagine that the interest rate on your savings account was 1% per year and inflation was 2% per year. After 1 year, would you be able to buy:"
+    )
+    q7 = models.IntegerField(
+        choices=[
+            (1, "True"),
+            (2, "False"),
+            (3, "Do not know/ refuse to answer"),
+        ],
+        widget=widgets.RadioSelectHorizontal,
+        label="Do you think that the following statement is true or false? Buying a single company stock usually provides a safer return than a stock mutual fund."
+    )
 
 # FUNCTIONS
 def creating_session(subsession: Subsession):
@@ -64,13 +126,14 @@ def creating_session(subsession: Subsession):
     investments_df = pd.read_csv(C.INVESTMENT_MATRIX_FILE)
     distributions_df = pd.read_csv(C.DISTRIBUTION_FILE)
 
-    # JSON strings
-    subsession.distributions = json.dumps(distributions_df.to_dict(orient='list'))
-    subsession.investment_matrix = json.dumps(investments_df.to_dict(orient='list'))
+    # Use a custom dictionary to store data in memory
+    subsession.session.vars['investments_data'] = investments_df.to_dict(orient='list')
+    subsession.session.vars['distributions_data'] = distributions_df.to_dict(orient='list')
+
 
 def calculate_lottery_return(player: Player):
-    distributions = json.loads(player.subsession.distributions)
-    investment_matrix = json.loads(player.subsession.investment_matrix)
+    distributions = player.session.vars['distributions_data']
+    investment_matrix = player.session.vars['investments_data']
 
     
     investment_amount = player.investment_amount
@@ -94,19 +157,24 @@ def calculate_lottery_return(player: Player):
     mean = int(column_name.split('mu')[1].split('sigma')[0])
     std_dv = int(column_name.split('sigma')[1].split('rf')[0])
 
+    try:
+        risky_draw = json.loads(player.risky_draw)
+    except json.JSONDecodeError:
+        risky_draw = []
+
+    # Append the new investment amount
+    risky_draw.append(round(random_return,3))
+
+    # Save the updated list back to the field
+    player.risky_draw = json.dumps(risky_draw)
+
     # Calculate returns
     risky_return = risky_investment * random_return
     risk_free_rate = int(column_name.split('rf')[-1]) / 100
     risk_free_return = (risk_free_investment) * (1 + risk_free_rate)
 
     outcome = risky_return + risk_free_return
-    
-    player.risky_investment = risky_investment
-    player.risk_free_investment = risk_free_investment
-    player.risky_return = risky_return
-    player.risk_free_return = risk_free_return
-    player.lottery_return = outcome
-    
+        
     # debug info
     print(f"Round {player.lottery_round} - Investment Amount: {100}")
     print(f"Risky Investment: {risky_investment}")
@@ -117,7 +185,7 @@ def calculate_lottery_return(player: Player):
     print(f"Risk-Free Rate: {risk_free_rate}")
     print(f"Risky Return: {risky_return}")
     print(f"Risk-Free Return: {risk_free_return}")
-    print(f"Total Return: {player.lottery_return}")
+    print(f"Total Return: {outcome}")
     print("\n")
 
     lottery = L(distribution_column, risk_free_rate, random_return, mean, std_dv, risky_investment, outcome)
@@ -140,7 +208,16 @@ class Intro(Page):
 
     @staticmethod
     def vars_for_template(player: Player):
-        return {}
+        if player.treatment == 'treatment_1':
+            treatment_value = 1
+        elif player.treatment == 'treatment_2':
+            treatment_value = 2
+        elif player.treatment == 'treatment_3':
+            treatment_value = 3
+
+        return {
+            'treatment': treatment_value,
+        }
     
 class InvestmentSelection(Page):
     form_model = 'player'
@@ -162,19 +239,22 @@ class InvestmentSelection(Page):
         return {
             'previous_investment': player.investment_amount if player.lottery_round > 1 else None,
         }
+    
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        # Load the current investment choices
+        try:
+            investment_choices = json.loads(player.investment_choices)
+        except json.JSONDecodeError:
+            investment_choices = []
 
+        # Append the new investment amount
+        investment_choices.append(player.investment_amount)
+
+        # Save the updated list back to the field
+        player.investment_choices = json.dumps(investment_choices)
     
 class Calibration(Page):
-    # @staticmethod
-    # def is_displayed(player: Player):
-    #     # Show InvestmentSelection only for the appropriate rounds
-    #     if player.treatment == 'treatment_2' and player.lottery_round % 2 == 1:
-    #         return True
-    #     elif player.treatment == 'treatment_3' and player.lottery_round % 4 == 1:
-    #         return True
-    #     elif player.treatment == 'treatment_1':
-    #         return True
-    #     return False
     @staticmethod
     def vars_for_template(player: Player):
         return {}
@@ -256,6 +336,7 @@ class Result(Page):
 
         return {
             **lottery_data,
+            'treatment': player.treatment,
             'total': 100 * len(last_lotteries),
             'lottery_size': len(last_lotteries),
             'cumulative_return': round(cumulative_return, 2),
@@ -267,7 +348,7 @@ class End(Page):
     def vars_for_template(player: Player):
         # Load the lotteries from the player
         lotteries = json.loads(player.lotteries) if player.lotteries else []
-        chosen_lotteries = json.loads(player.chosen_lotteries) if player.chosen_lotteries else []
+        chosen_indexes = json.loads(player.chosen_lotteries) if player.chosen_lotteries else []
 
         # Define block sizes based on treatment
         if player.treatment == 'treatment_1':
@@ -280,9 +361,9 @@ class End(Page):
             block_size = 4
             num_blocks = 1
 
-        if not player.chosen_lotteries or player.chosen_lotteries.strip() == '[]':
+        # Check if chosen_indexes is empty
+        if not chosen_indexes:
             chosen_indexes = []
-            chosen_lotteries = []
 
             # Choose blocks based on the treatment
             if player.treatment == 'treatment_1':
@@ -299,16 +380,11 @@ class End(Page):
             if len(chosen_indexes) > len(lotteries):
                 chosen_indexes = sorted(range(len(lotteries)))
 
-            # Collect the chosen lotteries
-            chosen_lotteries = [lotteries[i] for i in chosen_indexes]
-
-            # Save chosen lotteries to the player object
-            player.chosen_lotteries = json.dumps(chosen_lotteries)
+            # Save chosen indexes to the player object
+            player.chosen_lotteries = json.dumps(chosen_indexes)
         
-        else:
-            chosen_lotteries = json.loads(player.chosen_lotteries)
-            # Derive chosen indexes from chosen lotteries
-            chosen_indexes = [lotteries.index(lottery) for lottery in chosen_lotteries]
+        # Retrieve the chosen lotteries using the indexes
+        chosen_lotteries = [lotteries[i] for i in chosen_indexes]
 
         # Prepare the lottery data
         lottery_data = {}
@@ -322,18 +398,65 @@ class End(Page):
         payoff = cumulative_return * 0.02
         lottery_size = len(chosen_lotteries)
 
-        lottery_indexes = [i+1 for i in chosen_indexes]
-
         # Return the chosen lotteries and their data to the template
         return {
             **lottery_data,
             'num_blocks': num_blocks,
             'lottery_size': lottery_size,
-            'chosen_indexes': lottery_indexes,
+            'chosen_indexes': [i+1 for i in chosen_indexes],
             'blocks_indices': sorted(set((i // block_size) + 1 for i in chosen_indexes)),
             'cumulative_return': round(cumulative_return, 2),
             'payoff': round(payoff, 2),
         }
+    
+class TechQuestions(Page):
+    form_model = 'player'
+    form_fields = ['q1', 'q2', 'q3']
+    
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {}
+    
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        # Collect responses into a list
+        responses = [
+            player.q1,
+            player.q2,
+            player.q3
+        ]
+
+class RiskQuestions(Page):
+    form_model = 'player'
+    form_fields = ['q4']
+    
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {}
+    
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        # Collect responses into a list
+        responses = [
+            player.q4
+        ]
+
+class FinancialQuestions(Page):
+    form_model = 'player'
+    form_fields = ['q5', 'q6', 'q7']
+    
+    @staticmethod
+    def vars_for_template(player: Player):
+        return {}
+    
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        # Collect responses into a list
+        responses = [
+            player.q5,
+            player.q6,
+            player.q7
+        ]
 
 page_sequence = [
     Intro,
@@ -344,21 +467,24 @@ page_sequence = [
     InvestmentSelection, Lottery, Result,
 
     
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
     
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
 
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
-    InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
+    # InvestmentSelection, Lottery, Result,
 
     End,
+    TechQuestions,
+    RiskQuestions,
+    FinancialQuestions,
 ]
 
